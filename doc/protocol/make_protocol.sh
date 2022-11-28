830901ab5ac48d48bd9cd3f52f9d1e0dcf5fc723
Kleinholdermann <urs@kleinholdermann.de>
2022-11-28 02:28:30 +0100

    fixed issues with references, bibliography and footnotes

--- doc/protocol/make_protocol.sh
+++ doc/protocol/make_protocol.sh
@@ -4,5 +4,6 @@ rm *.log
 rm *.out
 
 pdflatex protocol-HessenKohorte2040.v1.0.tex
+bibtex protocol-HessenKohorte2040.v1.0.aux
 pdflatex protocol-HessenKohorte2040.v1.0.tex
 pdflatex protocol-HessenKohorte2040.v1.0.tex
